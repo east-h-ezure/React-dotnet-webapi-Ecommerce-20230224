77ca3bde71274cdb7578c57d50be41082af5a323
ezu0 <h-ezure@est.co.jp>
2023-02-27 14:13:56 +0900

    feature-2: add: reactの初期表示

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const products = [
+    {name: '走れメロス', price: 1500},
+    {name: '罪と罰', price: 1000}
+  ]
+
   return (
-    <div className="App">
-      hoge
+    <div>
+      <h1>本の森</h1>
+      <ul>
+        {
+          products.map((product) => (
+            <div>
+              {product.name} - {product.price}円
+            </div>
+          ))
+        }
+      </ul>
     </div>
-  );
+  )
 }
 
 export default App;
