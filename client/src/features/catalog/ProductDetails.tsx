ea2184f93571272d0b29b95164a6d3b5d7e7eb1a
ezu0 <h-ezure@est.co.jp>
2023-03-02 14:37:15 +0900

    add: detail page data

--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+import axios from 'axios';
+import { Product } from '../../product';
+import { Typography } from '@mui/material';
 
 const ProductDetails = () => {
-  return <div>ProductDetails</div>;
+  const { id } = useParams<{ id: string }>();
+  const [product, setProduct] = useState<Product | null>(null);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    axios
+      .get(`https://localhost:5000/api/products/${id}`)
+      .then((res) => setProduct(res.data))
+      .catch((error) => console.log(error))
+      .finally(() => setLoading(false));
+  }, [id]);
+  if (loading) return <h3>Loading...</h3>;
+  if (!product) return <h3>商品が見つかりません</h3>;
+  return (
+    <div>
+      <Typography variant="h2">{product.name}</Typography>
+    </div>
+  );
 };
 
 export default ProductDetails;
