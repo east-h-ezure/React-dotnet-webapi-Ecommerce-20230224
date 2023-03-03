948dd3ebf9a469fff383a9a1fe06c62af0475701
ezu0 <h-ezure@est.co.jp>
2023-03-03 17:36:48 +0900

    add:Bascket and BascketItem class, BascketController

---
+++ API/Controllers/BascketController.cs
@@ -0,0 +1,140 @@
+﻿using API.Models;
+using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BasketController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public BasketController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Bascket>> GetBascket()
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            var bascket = await connection.QueryFirstOrDefaultAsync<Bascket>(
+                   "SELECT * FROM Bascket " +
+                   "LEFT JOIN BascketItem ON Bascket.Id = BascketItem.BascketId " +
+                   "LEFT JOIN Product ON BascketItem.ProductId = Product.Id " +
+                   "WHERE Bascket.BuyerId = @buyerId",
+                   new { buyerId = Request.Cookies["buyerId"] });
+
+            if (bascket == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(bascket);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Product>> AddItemToBasket(int productId, int quantity)
+        {
+            var basket = await RetrieveBasket();
+            if (basket == null) basket = await CreateBasket();
+
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            connection.Open();
+            using var transaction = connection.BeginTransaction();
+            try
+            {
+                await connection.ExecuteAsync(
+                    "INSERT INTO BasketItem (ProductId, Quantity, BasketId) VALUES (@ProductId, @Quantity, @BasketId)",
+                    new { ProductId = productId, Quantity = quantity, BasketId = basket.Id },
+                    transaction: transaction);
+
+                transaction.Commit();
+
+                return StatusCode(201);
+            }
+            catch (Exception)
+            {
+                transaction.Rollback();
+                return BadRequest(new ProblemDetails { Title = "Problem adding item to the basket" });
+            }
+        }
+
+
+        [HttpDelete]
+        public async Task<ActionResult> RemoveBasketItem(int productId, int quantity = 1)
+        {
+            var basket = await RetrieveBasket();
+
+            if (basket == null) return NotFound();
+
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            basket.RemoveItem(productId, quantity);
+
+            var rowsAffected = await connection.ExecuteAsync(
+                "UPDATE BasketItems SET Quantity = @Quantity WHERE BasketId = @BasketId AND ProductId = @ProductId",
+                new { Quantity = basket.Items.Single(x => x.ProductId == productId).Quantity, BasketId = basket.Id, ProductId = productId });
+
+            if (rowsAffected > 0)
+            {
+                await connection.ExecuteAsync(
+                    "DELETE FROM BasketItems WHERE Quantity = 0 AND BasketId = @BasketId AND ProductId = @ProductId",
+                    new { BasketId = basket.Id, ProductId = productId });
+
+                return Ok();
+            }
+
+            return BadRequest(new ProblemDetails { Title = "Problem removing item from the basket" });
+        }
+
+
+
+
+        private async Task<Bascket> RetrieveBasket()
+        {
+            var buyerId = Request.Cookies["buyerId"];
+            if (buyerId == null) return null;
+
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            var basket = await connection.QueryFirstOrDefaultAsync<Bascket>(
+                "SELECT * FROM Basket WHERE BuyerId = @BuyerId",
+                new { BuyerId = buyerId });
+
+            if (basket != null)
+            {
+                var basketItems = await connection.QueryAsync<BascketItem>(
+                    "SELECT * FROM BasketItem WHERE BasketId = @BasketId",
+                    new { BasketId = basket.Id });
+                basket.Items = basketItems.ToList();
+            }
+
+            return basket;
+        }
+
+        private async Task<Bascket> CreateBasket()
+        {
+            var buyerId = Guid.NewGuid().ToString();
+            var cookieOptions = new CookieOptions { IsEssential = true, Expires = DateTime.Now.AddDays(30) };
+            Response.Cookies.Append("buyerId", buyerId, cookieOptions);
+
+            var basket = new Bascket { BuyerId = buyerId };
+
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync(
+                "INSERT INTO Basket (BuyerId) VALUES (@BuyerId)",
+                new { BuyerId = basket.BuyerId });
+
+            basket.Id = await connection.QuerySingleAsync<int>("SELECT @@IDENTITY");
+
+            return basket;
+        }
+    }
+}
---
+++ API/Models/Bascket.cs
@@ -0,0 +1,89 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace API.Models
+{
+    public class Bascket
+    {
+        public int Id { get; set; }
+        public string BuyerId { get; set; }
+        public List<BascketItem> Items { get; set; } = new();
+
+        private readonly IConfiguration _config;
+
+        public async Task AddItem(int productId, int quantity, SqlConnection connection)
+        {
+            // Get the product from the database
+            var product = await connection.QueryFirstOrDefaultAsync<Product>("SELECT * FROM Product WHERE Id = @ProductId", new { ProductId = productId });
+
+            // Create a new basket item and add it to the basket
+            var basketItem = new BascketItem
+            {
+                Quantity = quantity,
+                ProductId = productId,
+                Product = product
+            };
+            Items.Add(basketItem);
+        }
+
+        public void AddItem(Product product, int quantity)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            connection.Open();
+
+            // Check if the item is already in the basket
+            var existingItem = connection.QueryFirstOrDefault<BascketItem>(
+                @"SELECT * FROM BasketItem WHERE BasketId = @basketId AND ProductId = @productId",
+                new { basketId = Id, productId = product.Id });
+
+            if (existingItem == null)
+            {
+                // If the item is not in the basket, insert a new item
+                connection.Execute(
+                    @"INSERT INTO BasketItem (ProductId, BasketId, Quantity) VALUES (@productId, @basketId, @quantity)",
+                    new { productId = product.Id, basketId = Id, quantity });
+            }
+            else
+            {
+                // If the item is already in the basket, update the quantity
+                connection.Execute(
+                    @"UPDATE BasketItem SET Quantity = Quantity + @quantity WHERE BasketId = @basketId AND ProductId = @productId",
+                    new { basketId = Id, productId = product.Id, quantity });
+            }
+        }
+
+        public void RemoveItem(int productId, int quantity)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            connection.Open();
+
+            // Check if the item is in the basket
+            var item = connection.QueryFirstOrDefault<BascketItem>(
+                @"SELECT * FROM BasketItem WHERE BasketId = @basketId AND ProductId = @productId",
+                new { basketId = Id, productId });
+
+            if (item == null) return;
+
+            // Calculate the new quantity
+            var newQuantity = item.Quantity - quantity;
+
+            if (newQuantity <= 0)
+            {
+                // If the new quantity is less than or equal to 0, delete the item
+                connection.Execute(
+                    @"DELETE FROM BasketItem WHERE BasketId = @basketId AND ProductId = @productId",
+                    new { basketId = Id, productId });
+            }
+            else
+            {
+                // Otherwise, update the quantity
+                connection.Execute(
+                    @"UPDATE BasketItem SET Quantity = @newQuantity WHERE BasketId = @basketId AND ProductId = @productId",
+                    new { basketId = Id, productId, newQuantity });
+            }
+        }
+
+
+    }
+}
