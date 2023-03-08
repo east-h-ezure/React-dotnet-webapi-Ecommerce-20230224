475f17d976c3e6af2f04cd75e482a7c18031a639
ezu0 <h-ezure@est.co.jp>
2023-03-08 17:43:29 +0900

    fix: swaggerが使えない現象解決(同一HTTPメソッドとパスを使用していたせい)

--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -2,17 +2,17 @@
 
 namespace API.Controllers
 {
-    public class BuggyController: BaseApiController
-    {
-        [HttpGet]
-        public ActionResult GetNotFound()
-        {
-            return NotFound();
-        }
-        [HttpGet]
-        public ActionResult GetBadRequest()
-        {
-            return NotFound();
-        }
-    }
+    //public class BuggyController: BaseApiController
+    //{
+    //    [HttpGet]
+    //    public ActionResult GetNotFound()
+    //    {
+    //        return NotFound();
+    //    }
+    //    [HttpGet]
+    //    public ActionResult GetBadRequest()
+    //    {
+    //        return NotFound();
+    //    }
+    //}
 }
