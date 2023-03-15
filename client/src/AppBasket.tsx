2f4f780751ad97a534932d9a0cd055761aded9c7
ezu0 <h-ezure@est.co.jp>
2023-03-15 14:39:26 +0900

    update: 買い物かごUI変更(α版)

--- client/src/AppBasket.tsx
+++ client/src/AppBasket.tsx
@@ -1,6 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { BasketConfirm } from './app/models/basket';
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from '@mui/material';
 
 function AppBasket() {
   const [basketItems, setBasketItems] = useState<BasketConfirm[]>([]);
@@ -21,14 +30,43 @@ function AppBasket() {
   console.log(basketItems);
 
   return (
-    <ul>
-      {basketItems.map((item) => (
-        <li key={item.basketId}>
-          {item.product.name}: {item.quantity}
-          {item.product.brand}
-        </li>
-      ))}
-    </ul>
+    <div>
+      {/* <ul> */}
+      {/* {basketItems.map((item) => (
+          <li key={item.basketId}>
+            {item.product.name}: {item.quantity}
+            {item.product.brand}
+          </li>
+        ))}
+      </ul> */}
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell></TableCell>
+              <TableCell>商品名</TableCell>
+              <TableCell>数量</TableCell>
+              <TableCell>価格</TableCell>
+              <TableCell>削除</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {basketItems.map((item) => (
+              <TableRow
+                key={item.basketId}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell>{item.product.pictureUrl}</TableCell>
+                <TableCell>{item.product.name}</TableCell>
+                <TableCell>{item.quantity}</TableCell>
+                <TableCell>{item.product.price * item.quantity}</TableCell>
+                <TableCell>削除ボタン追加する</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
   );
 }
 
