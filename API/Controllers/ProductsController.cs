e34eefa671cef50cf0111dbe469a2ef24f599460
ezu0 <h-ezure@est.co.jp>
2023-03-31 14:04:20 +0900

    add: ProductのGETメソッドにページネーション機能追加

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -4,6 +4,7 @@ using Dapper;
 using Microsoft.AspNetCore.Mvc;
 using System.ComponentModel.DataAnnotations;
 using System.Data.SqlClient;
+using System.Linq.Dynamic.Core;
 using System.Text;
 
 namespace API.Controllers
@@ -45,60 +46,153 @@ namespace API.Controllers
         //}
 
         //ソートと検索の両方成功
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Product>>> GetProducts(
-            [FromQuery] string? sort,
-            [FromQuery] string? search
-            )
-        {
-            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
-            await connection.OpenAsync();
+        //[HttpGet]
+        //public async Task<ActionResult<IEnumerable<Product>>> GetProducts(
+        //    [FromQuery] string? sort,
+        //    [FromQuery] string? search
+        //    )
+        //{
+        //    using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+        //    await connection.OpenAsync();
 
-            using var transaction = await connection.BeginTransactionAsync();
+        //    using var transaction = await connection.BeginTransactionAsync();
 
-            try
-            {
-                var query = new StringBuilder("SELECT * FROM Product");
+        //    try
+        //    {
+        //        var query = new StringBuilder("SELECT * FROM Product");
 
-                //検索ワードがある場合
-                if (!string.IsNullOrEmpty(search))
-                {
-                    query.Append(" WHERE Name LIKE @SearchTerm");
-                }
+        //        //検索ワードがある場合
+        //        if (!string.IsNullOrEmpty(search))
+        //        {
+        //            query.Append(" WHERE Name LIKE @SearchTerm");
+        //        }
 
-                //ソートの条件
-                query.Append(sort switch
-                {
-                    "high-price" => " ORDER BY price DESC",
-                    "low-price" => " ORDER BY price ASC",
-                    _ => ""
-                });
-                var brandList = new List<string>();
-                var typeList = new List<string>();
+        //        //ソートの条件
+        //        query.Append(sort switch
+        //        {
+        //            "high-price" => " ORDER BY price DESC",
+        //            "low-price" => " ORDER BY price ASC",
+        //            _ => ""
+        //        });
+        //        var brandList = new List<string>();
+        //        var typeList = new List<string>();
 
-                //パラメーターの使用
-                var parameters = new DynamicParameters();
-                if (!string.IsNullOrEmpty(search))
-                {
-                    parameters.Add("@SearchTerm", $"%{search}%");
-                }
+        //        //パラメーターの使用
+        //        var parameters = new DynamicParameters();
+        //        if (!string.IsNullOrEmpty(search))
+        //        {
+        //            parameters.Add("@SearchTerm", $"%{search}%");
+        //        }
 
-                var products = await connection.QueryAsync<Product>(query.ToString(), parameters, transaction);
+        //        var products = await connection.QueryAsync<Product>(query.ToString(), parameters, transaction);
 
-                await transaction.CommitAsync();
+        //        await transaction.CommitAsync();
 
-                return Ok(products);
-            }
-            catch (Exception ex)
-            {
-                await transaction.RollbackAsync();
-                return BadRequest(new ProblemDetails
-                {
-                    Title = "Problem removing item to basket",
-                    Detail = $"Error occurred while executing command: {ex.Message}"
-                });
-            }
-        }
+        //        return Ok(products);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        await transaction.RollbackAsync();
+        //        return BadRequest(new ProblemDetails
+        //        {
+        //            Title = "Problem removing item to basket",
+        //            Detail = $"Error occurred while executing command: {ex.Message}"
+        //        });
+        //    }
+        //}
+        //    [HttpGet("{page}")]
+        //    public async Task<ActionResult<IEnumerable<Product>>> GetProducts(
+        //[FromQuery] string sort,
+        //[FromQuery] string search,
+        //[FromQuery] string page = "1",
+        //[FromQuery] string pageSize = "10"
+        //)
+        //    {
+        //        int pageNum = int.TryParse(page, out int p) ? p : 1;
+        //        int size = int.TryParse(pageSize, out int ps) ? ps : 10;
+
+        //        // 以下のコードは先程紹介したページネーションの例と同じです。
+        //        using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+        //        await connection.OpenAsync();
+
+        //        using var transaction = await connection.BeginTransactionAsync();
+
+        //        try
+        //        {
+        //            var query = new StringBuilder("SELECT * FROM Product");
+
+        //            if (!string.IsNullOrEmpty(search))
+        //            {
+        //                query.Append(" WHERE Name LIKE @SearchTerm");
+        //            }
+
+        //            query.Append(sort switch
+        //            {
+        //                "high-price" => " ORDER BY price DESC",
+        //                "low-price" => " ORDER BY price ASC",
+        //                _ => ""
+        //            });
+
+        //            var offset = (pageNum - 1) * size;
+        //            query = query.Append($" OFFSET {offset} ROWS FETCH NEXT {size} ROWS ONLY");
+
+        //            var parameters = new DynamicParameters();
+        //            if (!string.IsNullOrEmpty(search))
+        //            {
+        //                parameters.Add("@SearchTerm", $"%{search}%");
+        //            }
+
+        //            var products = await connection.QueryAsync<Product>(query.ToString(), parameters, transaction);
+
+        //            await transaction.CommitAsync();
+
+        //            return Ok(products);
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            await transaction.RollbackAsync();
+        //            return BadRequest(new ProblemDetails
+        //            {
+        //                Title = "Problem removing item to basket",
+        //                Detail = $"Error occurred while executing command: {ex.Message}"
+        //            });
+        //        }
+        //    }
+        //[HttpGet]
+        //public async Task<IActionResult> GetPagedProducts(int page, int pageSize)
+        //{
+        //    int skip = (page - 1) * pageSize;
+        //    int take = pageSize;
+        //    using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+        //    await connection.OpenAsync();
+        //    using var transaction = await connection.BeginTransactionAsync();
+
+        //    try
+        //    {
+        //                string sql = "SELECT * FROM Product ORDER BY Id OFFSET @Skip ROWS FETCH NEXT @Take ROWS ONLY";
+        //                var parameters = new { Skip = skip, Take = take };
+        //                var command = new SqlCommand(sql, connection, (SqlTransaction)transaction);
+        //                var products = await connection.QueryAsync<Product>(sql, parameters, transaction);
+
+        //                await transaction.CommitAsync();
+
+        //                return Ok(products);
+
+        //            }
+        //            catch (Exception ex)
+        //            {
+        //                await transaction.RollbackAsync();
+        //                return BadRequest(new ProblemDetails
+        //                {
+        //                    Title = "Problem getting paged products",
+        //                    Detail = $"Error occurred while executing command: {ex.Message}"
+        //                });
+        //            }
+        //        }
+
+
+
+
 
 
         //ソートと検索、フィルタリングの両方成功
@@ -166,6 +260,98 @@ namespace API.Controllers
         //        });
         //    }
         //}
+        //4つの機能を追加してテスト中
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts(
+    [FromQuery] string? sort,
+    [FromQuery] string? search,
+    [FromQuery] string? brands,
+    [FromQuery] string? types,
+    [FromQuery] int page = 1,
+    [FromQuery] int pageSize = 10
+)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.OpenAsync();
+
+            using var transaction = await connection.BeginTransactionAsync();
+
+            try
+            {
+                var query = new StringBuilder("SELECT * FROM Product");
+
+                var hasSearchTerm = !string.IsNullOrEmpty(search);
+                var hasBrand = !string.IsNullOrEmpty(brands);
+                var hasType = !string.IsNullOrEmpty(types);
+                var parameters = new DynamicParameters();
+
+                if (hasSearchTerm || hasBrand || hasType)
+                {
+                    query.Append(" WHERE");
+
+                    if (hasSearchTerm)
+                    {
+                        query.Append(" Name LIKE @SearchTerm");
+                        parameters.Add("@SearchTerm", $"%{search}%");
+                    }
+
+                    if (hasBrand)
+                    {
+                        query.Append(hasSearchTerm ? " AND" : "");
+                        query.Append(" Brand = @Brand");
+                        parameters.Add("@Brand", brands);
+                    }
+
+                    if (hasType)
+                    {
+                        query.Append((hasSearchTerm || hasBrand) ? " AND" : "");
+                        query.Append(" Type = @Type");
+                        parameters.Add("@Type", types);
+                    }
+                }
+
+                switch (sort)
+                {
+                    case "name_asc":
+                        query.Append(" ORDER BY Name ASC");
+                        break;
+                    case "name_desc":
+                        query.Append(" ORDER BY Name DESC");
+                        break;
+                    case "price_asc":
+                        query.Append(" ORDER BY Price ASC");
+                        break;
+                    case "price_desc":
+                        query.Append(" ORDER BY Price DESC");
+                        break;
+                    default:
+                        query.Append(" ORDER BY Id ASC");
+                        break;
+                }
+
+                int skip = (page - 1) * pageSize;
+                int take = pageSize;
+                query.Append(" OFFSET @Skip ROWS FETCH NEXT @Take ROWS ONLY");
+                parameters.Add("@Skip", skip);
+                parameters.Add("@Take", take);
+
+                var products = await connection.QueryAsync<Product>(query.ToString(), parameters, transaction);
+
+                await transaction.CommitAsync();
+
+                return Ok(products);
+            }
+            catch (Exception ex)
+            {
+                await transaction.RollbackAsync();
+                return BadRequest(new ProblemDetails
+                {
+                    Title = "Problem getting products",
+                    Detail = $"Error occurred while executing command: {ex.Message}"
+                });
+            }
+        }
+
 
         [HttpGet("{productId}")]
         public async Task<ActionResult<Product>> GetProduct(int productId)
